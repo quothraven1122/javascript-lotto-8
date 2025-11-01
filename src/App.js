5883698037f6afd5c38e4e14084a0173568036e9
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2025-11-01 19:47:07 +0900

    feat: add error handling to getLottoCount

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import { Console, MissionUtils } from "@woowacourse/mission-utils";
 
 const getLottoCount = async () => {
-  const money = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
-  return money / 1000;
+  while (true) {
+    const money = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+    try {
+      checkMoneyInput(money);
+    } catch (e) {
+      Console.print(e.message);
+      continue;
+    }
+    return money / 1000;
+  }
 };
-
+const checkMoneyInput = (money) => {
+  if (isNaN(money))
+    throw new Error("[ERROR] Input Error: Money Needs to be Int");
+  if (money % 1000 !== 0)
+    throw new Error("[ERROR] Input Error: Money Needs to be Divided by 1000");
+};
 class App {
   async run() {
     const lottoCount = await getLottoCount();
