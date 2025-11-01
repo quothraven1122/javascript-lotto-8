356167fb3fb317e8f274621fe7eb97f10ea6df71
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2025-11-01 19:23:16 +0900

    feat: add function that gets count of lotto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+
+const getLottoCount = async () => {
+  const money = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+  return money / 1000;
+};
+
 class App {
-  async run() {}
+  async run() {
+    const lottoCount = await getLottoCount();
+  }
 }
 
 export default App;
