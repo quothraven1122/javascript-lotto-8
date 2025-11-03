23ba23ce1811d3621b6647cfa4d1ee43cdc073be
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2025-11-03 17:43:48 +0900

    refactor: restructure error.js

--- src/error.js
+++ src/error.js
@@ -1,9 +1,20 @@
+//공통 에러 확인
+const checkNumber = (num) => {
+  if (isNaN(num))
+    throw new Error("[ERROR] Input Error: Money must be a number");
+  if (!Number.isInteger(num))
+    throw new Error("[ERROR] Input Error: Money must be an integer");
+};
+const checkLottoNumbers = (num) => {
+  checkNumber(num);
+  if (num > 45 || num < 1)
+    throw new Error("[ERROR] Input Error: Number out of bounds");
+};
+
+//특수 에러 확인
 export const checkMoneyInput = (num) => {
   const money = Number(num);
-  if (isNaN(money))
-    throw new Error("[ERROR] Input Error: Money must be a number");
-  if (!Number.isInteger(money))
-    throw new Error("[ERROR] Input Error: Money must be an integer");
+  checkNumber(money);
   if (money % 1000 !== 0)
     throw new Error("[ERROR] Input Error: Money must be divided by 1000");
 };
@@ -11,24 +22,14 @@ export const checkWinningNumbers = (winningNumbers) => {
   if (winningNumbers.length !== 6)
     throw new Error("[ERROR] Input Error: Must be 6 numbers");
   winningNumbers.forEach((num) => {
-    if (isNaN(num))
-      throw new Error("[ERROR] Input Error: Number must be a number");
-    if (!Number.isInteger(num))
-      throw new Error("[ERROR] Input Error: Number must be an integer");
-    if (num > 45 || num < 1)
-      throw new Error("[ERROR] Input Error: Number out of bounds");
+    checkLottoNumbers(num);
   });
   if (new Set(winningNumbers).size !== winningNumbers.length)
     throw new Error("[ERROR] Input Error: Duplicate numbers are not allowed");
 };
 export const checkBonusNumber = (num, winningNumbers) => {
   const bonusNumber = Number(num);
-  if (isNaN(bonusNumber))
-    throw new Error("[ERROR] Input Error: Number must be a number");
-  if (!Number.isInteger(bonusNumber))
-    throw new Error("[ERROR] Input Error: Number must be an integer");
-  if (bonusNumber > 45 || bonusNumber < 1)
-    throw new Error("[ERROR] Input Error: Number out of bounds");
+  checkLottoNumbers(bonusNumber);
   if (winningNumbers.includes(bonusNumber))
     throw new Error("[ERROR] Input Error: Duplicate numbers are not allowed");
 };
