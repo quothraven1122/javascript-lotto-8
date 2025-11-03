774589b289f315931601b3f8d47b7bd061b181b1
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2025-11-03 18:47:40 +0900

    refactor: move lotto logic to Lotto.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
 import {
   getLottoCount,
   printLottoCount,
@@ -7,56 +6,22 @@ import {
   getBonusNumber,
   printWinStats,
 } from "./io.js";
+import Lotto from "./Lotto.js";
 
-const LottoRank = [
-  { rank: 5, match: 3, prize: 5000, count: 0 },
-  { rank: 4, match: 4, prize: 50000, count: 0 },
-  { rank: 3, match: 5, prize: 1500000, count: 0 },
-  { rank: 2, match: 5, bonus: true, prize: 30000000, count: 0 },
-  { rank: 1, match: 6, prize: 2000000000, count: 0 },
-];
-
-const getLottoNumbers = (lottoCount) => {
-  return Array.from({ length: lottoCount }, () =>
-    MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b)
-  );
-};
-
-const getWinCount = (lottoNumbers, winningNumbers, bonusNumber) => {
-  //[5등, 4등, 3등, 2등, 1등]
-  lottoNumbers.forEach((lottoPaper) => {
-    const matchCount = lottoPaper.filter((num) =>
-      winningNumbers.includes(num)
-    ).length;
-    if (matchCount === 6) LottoRank[4].count++;
-    else if (matchCount === 5 && lottoPaper.includes(bonusNumber))
-      LottoRank[3].count++;
-    else if (matchCount === 5) LottoRank[2].count++;
-    else if (matchCount === 4) LottoRank[1].count++;
-    else if (matchCount === 3) LottoRank[0].count++;
-  });
-};
-const calculateReturn = (moneyUsed) => {
-  let moneyEarned = 0;
-  LottoRank.forEach((rank) => {
-    moneyEarned += rank.prize * rank.count;
-  });
-  return parseFloat(((moneyEarned / moneyUsed) * 100).toFixed(2));
-};
 class App {
   async run() {
     const lottoCount = await getLottoCount();
     printLottoCount(lottoCount);
 
-    const lottoNumbers = getLottoNumbers(lottoCount);
-    printLottoNumbers(lottoNumbers);
+    const lotto = new Lotto(lottoCount);
+    printLottoNumbers(lotto.numbers);
 
     const winningNumbers = await getWinningNumbers();
     const bonusNumber = await getBonusNumber(winningNumbers);
 
-    getWinCount(lottoNumbers, winningNumbers, bonusNumber);
+    lotto.evaluate(winningNumbers, bonusNumber);
 
-    printWinStats(LottoRank, calculateReturn(lottoCount * 1000));
+    printWinStats(lotto.rankings, lotto.calculateReturn());
   }
 }
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,54 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class Lotto {
-  #numbers;
+  #numbers = [];
+  #rankings = [
+    { rank: 5, match: 3, prize: 5000, count: 0 },
+    { rank: 4, match: 4, prize: 50000, count: 0 },
+    { rank: 3, match: 5, prize: 1500000, count: 0 },
+    { rank: 2, match: 5, bonus: true, prize: 30000000, count: 0 },
+    { rank: 1, match: 6, prize: 2000000000, count: 0 },
+  ];
 
-  constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = numbers;
+  constructor(count) {
+    this.#numbers = Array.from({ length: count }, () =>
+      MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort(
+        (a, b) => a - b
+      )
+    );
   }
 
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+  evaluate(winningNumbers, bonusNumber) {
+    this.#numbers.forEach((lottoPaper) => {
+      const matchCount = lottoPaper.filter((num) =>
+        winningNumbers.includes(num)
+      ).length;
+
+      if (matchCount === 6) this.#rankings[4].count++;
+      else if (matchCount === 5 && lottoPaper.includes(bonusNumber))
+        this.#rankings[3].count++;
+      else if (matchCount === 5) this.#rankings[2].count++;
+      else if (matchCount === 4) this.#rankings[1].count++;
+      else if (matchCount === 3) this.#rankings[0].count++;
+    });
   }
 
-  // TODO: 추가 기능 구현
+  calculateReturn() {
+    const totalSpent = this.#numbers.length * 1000;
+    const totalEarned = this.#rankings.reduce(
+      (sum, r) => sum + r.prize * r.count,
+      0
+    );
+    return parseFloat(((totalEarned / totalSpent) * 100).toFixed(2));
+  }
+
+  get numbers() {
+    return this.#numbers;
+  }
+
+  get rankings() {
+    return this.#rankings;
+  }
 }
 
 export default Lotto;
